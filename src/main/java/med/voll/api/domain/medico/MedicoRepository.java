9b39141b7cc6261924d067d8ad11f83b40f78521
Gedione Souza <gedione.souza@certacon.com.br>
2024-03-25 09:29:37 -0300

    test: request with error 400

--- src/main/java/med/voll/api/domain/medico/MedicoRepository.java
+++ src/main/java/med/voll/api/domain/medico/MedicoRepository.java
@@ -27,7 +27,8 @@ public interface MedicoRepository extends JpaRepository<Medico, Long> {
             order by rand()
             limit 1
     """)
-    Medico escolherMedicoAleatorioLivreNaData(Especialidade especialidade, LocalDateTime data);
+
+    Medico  escolherMedicoAleatorioLivreNaData(Especialidade especialidade, LocalDateTime data);
 
     @Query("""
             select m.ativo
